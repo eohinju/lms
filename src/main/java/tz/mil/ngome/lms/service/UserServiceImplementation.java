1e3f25007c6fadca74980ee987bf97fa851c1db6
elly <ellyjulias@gmail.com>
2021-01-06 09:02:18 +0300

    class service  correction

--- src/main/java/tz/mil/ngome/lms/service/UserServiceImplementation.java
+++ src/main/java/tz/mil/ngome/lms/service/UserServiceImplementation.java
@@ -1,26 +1,24 @@
 package tz.mil.ngome.lms.service;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import tz.mil.ngome.lms.dto.MemberDto;
-import tz.mil.ngome.lms.entity.Member;
-import tz.mil.ngome.lms.repository.MemberRepository;
+import tz.mil.ngome.lms.dto.UserDto;
+import tz.mil.ngome.lms.entity.User;
+import tz.mil.ngome.lms.repository.UserRepository;
 
 @Service
-public class ServiceServiceImplementation implements ServiceService {
+public class UserServiceImplementation implements UserService {
 
 	@Autowired
-	MemberRepository memberRepo;
+	UserRepository userRepo;
 	
 	@Override
-	public Member registerMember(MemberDto memberDto) {
-		Member member = new Member();
-		BeanUtils.copyProperties(memberDto, member, "id");
-		member.setCreatedBy("System");
-		Member savedMember = memberRepo.save(member);
-		return savedMember;
+	public User registerUser(UserDto userDto) {
+		User user = new User();
+		BeanUtils.copyProperties(userDto, user, "id");
+		user.setCreatedBy("System");
+		User savedUser = userRepo.save(user);
+		return savedUser;
 	}
 
 }
