162d430ec44b4fc954df7842ce14b746339b21e4
Emmanuel Hinju <eohinju@gmail.com>
2021-01-27 18:37:45 +0300

    Implemented paging for getMembers()

--- src/main/java/tz/mil/ngome/lms/controller/MemberController.java
+++ src/main/java/tz/mil/ngome/lms/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -64,7 +65,7 @@ public class MemberController {
 	private Response<Page<MemberDto>> getMembers(@RequestParam(name = "page", required = false) Integer page,@RequestParam(name = "size", required = false) Integer size) {
 		int p = page==null || page.intValue()<0?0:page.intValue();
 		int s = size==null || size.intValue()<0?conf.getDefaultPageSize():size.intValue();
-		Pageable pageable = PageRequest.of(p, s);
+		Pageable pageable = PageRequest.of(p, s, Sort.by(Sort.Order.asc("compnumber")));
 		return this.memberService.getMembers(pageable);
 	}
 	
