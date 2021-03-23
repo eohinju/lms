876407e04050d1ba217011370c0ac36e59a244af
Emmanuel Hinju <eohinju@gmail.com>
2021-03-23 11:21:09 +0300

    progress

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -3,46 +3,56 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "5e9b2a547e4a9cf3519fe57b7cab20a3",
+  "assets/AssetManifest.json": "8add73a1677d056224cd19f6a92301a1",
 "assets/assets/account.svg": "ea675ac1c8d3a7285a7962f6958c5d1c",
+"assets/assets/accountBlack.svg": "ea675ac1c8d3a7285a7962f6958c5d1c",
+"assets/assets/accountSelect.svg": "ea675ac1c8d3a7285a7962f6958c5d1c",
 "assets/assets/activity.svg": "6ced257d98e7e4ee58745e46682d526f",
+"assets/assets/activitySelect.svg": "6ced257d98e7e4ee58745e46682d526f",
 "assets/assets/admin.svg": "74af450a16e08d9100c02841c8e7cd46",
 "assets/assets/approved.svg": "d3432c7d8796af21aef614067490435a",
 "assets/assets/authorized.svg": "b574766f75e07b9bdefdee81d65d616b",
+"assets/assets/Barack_Obama.pdf": "09ac03cad84116683495369142e61d9d",
 "assets/assets/borrowers.svg": "ce9ccf3236dc005af4f1f98a9683f38b",
 "assets/assets/cashRegister.svg": "90ba8dc49dea6baa90534cc130c93833",
 "assets/assets/coinImage.jpg": "5ac509aa32896561644a206f2657caf6",
 "assets/assets/customer.svg": "26f6a773184d46c6670ca21efceb3687",
 "assets/assets/dashboard.svg": "e8218df4e44a1cd48c62fbec9bbabf0d",
+"assets/assets/dashboardSelect.svg": "e8218df4e44a1cd48c62fbec9bbabf0d",
 "assets/assets/disburse.svg": "c900adc8636861e3b9b7e4cff96d278c",
+"assets/assets/disburseBlack.svg": "c900adc8636861e3b9b7e4cff96d278c",
+"assets/assets/disburseSelect.svg": "c900adc8636861e3b9b7e4cff96d278c",
 "assets/assets/expenses.svg": "a68cadd4e2cd6f99d959e4621d1d6e9b",
+"assets/assets/expensesIconBlack.svg": "a68cadd4e2cd6f99d959e4621d1d6e9b",
+"assets/assets/expensesSelect.svg": "a68cadd4e2cd6f99d959e4621d1d6e9b",
 "assets/assets/importMembers.svg": "e7c75111bd2bed7eafd5a074f3e3c088",
 "assets/assets/loanType.svg": "b6de564b857880aa20a3d8bb5eafd3fc",
 "assets/assets/logout.svg": "1a5bd42f5dbff19dc9f336a02bdffe9c",
 "assets/assets/member.svg": "28f3555c53a8166fd58940039be76dfa",
 "assets/assets/members.svg": "31f258ce8d93e83603df5ab76d4bda9a",
+"assets/assets/memberSelect.svg": "28f3555c53a8166fd58940039be76dfa",
 "assets/assets/menu.svg": "820124e9c560f84aeadbc4da474ba7ca",
 "assets/assets/password.svg": "0ddc1e94ca6b7e7793d20ac8b3af3327",
 "assets/assets/profile.svg": "31ce89ddc6e7af6b59977800f44702bd",
 "assets/assets/refund.svg": "212960b44bf99b2a16554566e4c881d6",
 "assets/assets/reports.svg": "a10a72b411963b93dafe083371ae3fe1",
 "assets/assets/request.svg": "f20f85856584fc022288ac77b3f2c8fc",
-"assets/assets/returns.csv": "c07fbff429e800c439dc4b9244386522",
-"assets/assets/sample.pdf": "4b41a3475132bd861b30a878e30aa56a",
 "assets/assets/search.svg": "67f82549a0c8ad3a3c97160014bf1065",
+"assets/assets/test.html": "4ee7c72baefd39d1693df660ed0ca268",
 "assets/assets/username.svg": "b6c907597490e15e371e1b916dc3aba5",
 "assets/assets/webIcon.png": "384520cce715b1e3fb38310ce7ebcbeb",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "e3c1a8ab9a227cd5c19297ee0f4a8bdb",
+"assets/NOTICES": "5247e8f9a5ac0a489c7531749c7bbd65",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "384520cce715b1e3fb38310ce7ebcbeb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5e464ff7d5384e6feaaf96c448751d0a",
-"/": "5e464ff7d5384e6feaaf96c448751d0a",
-"main.dart.js": "43fa963a4da1e9fcfb348a6bc31636bb",
+"index.html": "753fe8c35d6455bdb38d479bc48bf2fc",
+"/": "753fe8c35d6455bdb38d479bc48bf2fc",
+"main.dart.js": "ad922e88f026467ef527798b59d52ee5",
 "manifest.json": "e718771eee720531a41189805b789b77",
+"pdfFunction.js": "a3c628e9c35bd599f15761ba98d20ebd",
 "splash/style.css": "71533778dc93ea83ad31bb3835b3f48b",
 "version.json": "34f343e0052c946c4cc166ba681fed11"
 };
@@ -188,7 +198,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
